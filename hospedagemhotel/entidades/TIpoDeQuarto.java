4a2d0d8931b35c44a66bdec0d613b06c07d6f26e
Vagner Simone Martins Filho <estagioeducacao6@sesims.com.br>
2021-06-01 16:40:46 -0300

    tipo de quarto

---
+++ hospedagemhotel/entidades/TIpoDeQuarto.java
@@ -0,0 +1,22 @@
+package entidades;
+
+public class TIpoDeQuarto {
+    private float valor;
+    private String descricao;
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public float getValor() {
+        return valor;
+    } 
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
