aeb32fb0c9414e9c87582623b3942c94c5c42072
Henrique <henriquemilitao35@gmail.com>
2021-06-10 02:39:06 -0300

    Alterações na classe Quarto - Nada demais aqui

--- src/hospedagemhotel/entidades/Quarto.java
+++ src/hospedagemhotel/entidades/Quarto.java
@@ -5,12 +5,11 @@ import java.util.Scanner;
 public class Quarto {
     private int codigoQuarto, localizacao, situacao; //SituaÁ„o 0 n„o est„o usando / SituaÁ„o 1 est„o usando
     private Hospedagem hospedagem;
+    private TipoDeQuarto tipoDeQuarto;
 
-    Scanner leitor = new Scanner(System.in);
-    
-    public Quarto(int codigoQuarto) {
-    	setCodigoQuarto(codigoQuarto);
-    }
+    public Quarto() {
+		
+	}
 
     public Quarto(int codigoQuarto, int localizacao, int situacao) {
         setCodigoQuarto(codigoQuarto);
@@ -43,11 +42,15 @@ public class Quarto {
         return situacao;
     }
 
-    // Par√¢metro da fun√ß√£o setSitua√ß√£o pode mudar para atender diferentes situa√ß√µes de um quarto
+    // Parametro da funcao setSituacao pode mudar para atender diferentes situacoes de um quarto
     public void mudarSituacaoQuarto(Quarto quarto) {
-        System.out.println("Informe a nova situa√ß√£o do quarto: ");
+    	Scanner leitor = new Scanner(System.in);
+    	
+        System.out.println("Informe a nova situacao do quarto: ");
         int situacao = leitor.nextInt();
         quarto.setSituacao(situacao);
+        
+        leitor.close();
     }
 
 	public Hospedagem getHospedagem() {
@@ -59,7 +62,7 @@ public class Quarto {
 	}
     
 	public String toString() {
-		return "CÛdigo: " + codigoQuarto + "\nLocalizacao: " + localizacao + 
+		return "Codigo: " + codigoQuarto + "\nLocalizacao: " + localizacao + 
 				"\nSituacao: " + situacao + "\nHospedagem: " + hospedagem; 
 	}
     
