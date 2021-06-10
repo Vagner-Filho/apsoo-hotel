25a1754c72101a62e06b858f95c7947fcfb9609f
Henrique <henriquemilitao35@gmail.com>
2021-06-10 02:21:39 -0300

    Alterações na classe Hospede - Exclui mais um construtor de 50 parametros e tirei o atributo hospedagem no construtor por enquanto

--- src/hospedagemhotel/entidades/Hospede.java
+++ src/hospedagemhotel/entidades/Hospede.java
@@ -6,24 +6,10 @@ public class Hospede extends Pessoa {
 	private int codigoConta;
 	private Hospedagem hospedagem;
 	
-	public Hospede(String nome, String cpf, int telefone, int dia, int mes, int ano, 
-			String rua, String bairro, String cidade, String estado, String complemento, 
-			int numero, int cep, String sexo, int codigoConta) {
-		super(nome, cpf, telefone, dia, mes, ano, rua, bairro, cidade, 
-				estado, complemento, numero, cep);
-		setSexo(sexo);
-		setCodigoConta(codigoConta);
-	}
 	
 	public Hospede(String nome, String cpf, int telefone, Date dataNasc, 
-			Endereco endereco, String sexo, int codigoConta, Hospedagem hospedagem) {
+			Endereco endereco, String sexo, int codigoConta) {
 		super(nome, cpf, telefone, dataNasc, endereco);
-		setSexo(sexo);
-		setCodigoConta(codigoConta);
-		setHospedagem(hospedagem);
-	}
-	
-	public Hospede(String sexo, int codigoConta) {
 		setSexo(sexo);
 		setCodigoConta(codigoConta);
 	}
